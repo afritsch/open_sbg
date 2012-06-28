d8bcad14100aca53331db489f704a8ffa53efa26
Andreas Fritsch <fritschandreas@hotmail.com>
2012-06-28 11:44:57 +0200

    initial commit, start coords+zoom set

---
+++ logic.js
@@ -0,0 +1,9 @@
+﻿function initialize() {
+  var myOptions = {
+    center: new google.maps.LatLng(47.80949,13.05501),
+    zoom: 14,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  };
+  var map = new google.maps.Map(document.getElementById("map_canvas"),
+      myOptions);
+}
