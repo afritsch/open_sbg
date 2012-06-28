f6ee10e508ab95254678d515703e56271f318d1a
Sören Hentzschel <kontakt@soeren-hentzschel.at>
2012-06-28 14:47:13 +0200

    use original gml file (xml) and place markers

--- logic.js
+++ logic.js
@@ -1,21 +1,67 @@
 ﻿$(document).ready(function() {
-  var myLatLng = new google.maps.LatLng(47.80949,13.05501);
+  /* map configuration */
+  var selector  = document.getElementById('map_canvas');
+  var coords    = new google.maps.LatLng(47.80949,13.05501);
+  var zoom      = 14;
+  var type      = google.maps.MapTypeId.HYBRID;
+  var xmlFile   = 'open_sbg.xml'
   
-  OPENSBG.init(document.getElementById("map_canvas"), myLatLng, 14);
+  /* initialize map and place markers */
+  OpenSBG.init(selector, coords, zoom, type);
+  OpenSBG.placeMarkers(xmlFile);
 });
 
-var OPENSBG = {
+var OpenSBG = {
   map: null,
-	bounds: null
-}
-
-OPENSBG.init = function(selector, latLng, zoom) {
-  var myOptions = {
-    zoom:zoom,
-    center: latLng,
-    mapTypeId: google.maps.MapTypeId.HYBRID
-  }
-  
-  this.map = new google.maps.Map($(selector)[0], myOptions);
-	this.bounds = new google.maps.LatLngBounds();
+	bounds: null,
+	
+	init : function(selector, coords, zoom, type) {
+  	var options = {
+      zoom: zoom,
+      center: coords,
+      mapTypeId: type
+    };
+    
+    this.map = new google.maps.Map(selector, options);
+    this.bounds = new google.maps.LatLngBounds();
+	},
+	
+	placeMarkers : function(filename) {
+  	$.get(filename, function(xml) {
+  		$(xml).find('gml\\:featureMember').each(function() {
+  			/* read info */
+  			var bereich = $(this).find('gmgml\\:BEREICH').text();
+  			var bezeichnung = $(this).find('gmgml\\:BEZEICHNUNG').text();
+  			var adresse = $(this).find('gmgml\\:ADRESSE').text();
+  			var beschreibung = $(this).find('gmgml\\:BESCHREIBUNG').text();
+  			
+  			/* determine latitude and longitude */
+  			var position = $(this).find('gml\\:pos').text();
+  			var delimiter = pos.indexOf(' ');
+  			
+  			var lat = pos.substr(0, delimiter);
+  			var lng = pos.substr(delimiter + 1, pos.length);
+  			
+  			/* place marker */
+  			var point = new google.maps.LatLng(parseFloat(lat), parseFloat(lng));
+  			OpenSBG.bounds.extend(point);
+  			
+  			var marker = new google.maps.Marker({
+  				position: point,
+  				map: OpenSBG.map
+  			});
+  			
+  			/* overlay window */
+  			var infoWindow = new google.maps.InfoWindow();
+  			var html = '<strong>' + bezeichnung + '</strong.><br />' + adresse;
+  			
+  			google.maps.event.addListener(marker, 'click', function() {
+  				infoWindow.setContent(html);
+  				infoWindow.open(OpenSBG.map, marker);
+  			});
+  			
+  			//OpenSBG.map.fitBounds(OpenSBG.bounds);
+  		});
+  	});
+	}
 }
